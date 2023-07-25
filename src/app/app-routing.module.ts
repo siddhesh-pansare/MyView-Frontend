87fb1270db18e458fbe9e4e5f770e33b5bd99234
Shivanshu Khanna <Shivanshu.Khanna@Geminisolutions.com>
2023-07-25 11:44:41 +0530

    adding sidebar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './modules/shared-modules/shared-components/login/login.component';
+import { SideBarComponent } from './modules/shared-modules/shared-components/side-bar/side-bar.component';
 
 const routes: Routes = [
-  { path:'', redirectTo: 'login', pathMatch:'full' },
-  { path:'login', component:LoginComponent }
+  { path:'', redirectTo: 'side-bar', pathMatch:'full' },
+  { path:'side-bar', component:SideBarComponent }
 ];
 
 @NgModule({

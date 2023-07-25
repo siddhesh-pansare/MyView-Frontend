fc7319f2c01589b776f48a2acddc1d2b9efe5fe8
siddhesh-pansare <122260522+siddhesh-pansare@users.noreply.github.com>
2023-07-25 14:41:40 +0530

    data loaded

--- src/app/modules/employee/dashboard/dashboard.component.ts
+++ src/app/modules/employee/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
-import { HttpClient } from '@angular/common/http';
+
 import { Component, OnInit } from '@angular/core';
-import { map } from 'rxjs/operators';
+
+import { LoggedUserDataService } from 'src/app/services/logged-user-data.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -11,37 +12,19 @@ export class DashboardComponent implements OnInit {
 
   //declare variables
   loggedUserData: any;     //variable to store complete data from MIS
-
-  constructor(private http: HttpClient){}
+  constructor(private userDataService: LoggedUserDataService ){}
 
   ngOnInit(): void {
-    this.getDataFromMisApi();
+    this.loadData();
   }
 
-  getDataFromMisApi(){
-    const apiUrl = 'https://misapi.geminisolutions.com/api/External/OrgDetails?key=Ks5GSWiV2tZjuF87rSIJgQ==&token=9849AE58-1CF2-4D97-8F1A-5B4D7A52BAA5';
-    this.http.get<any>(apiUrl).pipe(
-      map((data) => this.filterDataByEmployeeEmail(data.Result,'siddhesh.pansare@geminisolutions.com'))
-    ).subscribe((filteredData)=>{
+  loadData(): void {
+    const email = 'siddhesh.pansare@geminisolutions.com';
+
+    this.userDataService.fetchDataByEmail(email).subscribe((filteredData) => {
       this.loggedUserData = filteredData[0];
+      //console.log('here', this.loggedUserData);
     });
-
-
-
   }
-
-  //function to filter complete mis data to particular user
-  filterDataByEmployeeEmail(data: any, employeeEmail: string): any{
-    return data.filter((item:any) => item.EmailId === employeeEmail)
-  }
-
-  // getProfileUrl(){
-  //   const matchingUser = this.detailsArray.find(user => user.EmailId === this.userId);
-  // if (matchingUser) {
-  //   localStorage.setItem('imagePath', matchingUser.ImagePath);
-  //   this.imagePath = localStorage.getItem('imagePath')
-  // } else {
-  //   console.log('Email not found');
-  // }
 
 }

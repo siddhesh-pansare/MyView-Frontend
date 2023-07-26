9f772277a6a8c8ff47f5b803e5ab8cdbc418e308
Shobhit Singh <Shobhit.Singh@Geminisolutions.com>
2023-07-26 12:43:30 +0530

    search bar transition done

--- src/app/modules/shared-modules/shared-components/header/header.component.ts
+++ src/app/modules/shared-modules/shared-components/header/header.component.ts
@@ -9,5 +9,22 @@ import { Component} from '@angular/core';
   
 })
 export class HeaderComponent {
-  
+  isDropdownOpen = false;
+
+  constructor() { }
+
+  toggleDropdown() {
+    this.isDropdownOpen = !this.isDropdownOpen;
+}
+
+isSearchBarVisible = false;
+
+  showSearchBar() {
+    this.isSearchBarVisible = true;
+  }
+
+  hideSearchBar() {
+    this.isSearchBarVisible = false;
+  }
+
 }

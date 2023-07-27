218555b90efff39d4be6696dd1cf798ae43a6211
siddhesh-pansare <122260522+siddhesh-pansare@users.noreply.github.com>
2023-07-27 10:35:33 +0530

    lg screen view

--- src/app/modules/employee/dashboard/dashboard.component.ts
+++ src/app/modules/employee/dashboard/dashboard.component.ts
@@ -12,6 +12,16 @@ export class DashboardComponent implements OnInit {
 
   //declare variables
   loggedUserData: any;     //variable to store complete data from MIS
+
+  cards: { cardImageSrc: string; cardTitle: string, ImageAlt: string }[] = [
+    { cardImageSrc: '../../../../assets/images/icons/awards.svg', cardTitle: 'Awards', ImageAlt:'award' },
+    { cardImageSrc: '../../../../assets/images/icons/certificate.svg', cardTitle: 'Certifications', ImageAlt:'certification' },
+    { cardImageSrc: '../../../../assets/images/icons/interview.svg', cardTitle: 'Interviews', ImageAlt:'interviews' },
+    { cardImageSrc: '../../../../assets/images/icons/session.svg', cardTitle: 'Sessions', ImageAlt:'sessions' },
+    { cardImageSrc: '../../../../assets/images/icons/client_feedback.svg', cardTitle: 'Client Feedback', ImageAlt:'client_feedback' },
+    { cardImageSrc: '../../../../assets/images/icons/other_contributions.svg', cardTitle: 'Other Contributions', ImageAlt:'other-Contributions' },
+  ];
+
   constructor(private userDataService: LoggedUserDataService ){}
 
   ngOnInit(): void {
--- src/app/modules/employee/dashboard/ec-information/ec-information.component.ts
+++ src/app/modules/employee/dashboard/ec-information/ec-information.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { LoggedUserDataService } from 'src/app/services/logged-user-data.service';
 
 @Component({
   selector: 'app-ec-information',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class ECInformationComponent {
 
+  loggedUserData: any;     //variable to store complete data from MIS
+
+  constructor(private userDataService: LoggedUserDataService ){
+    const email = 'siddhesh.pansare@geminisolutions.com';
+
+    this.userDataService.fetchDataByEmail(email).subscribe((filteredData) => {
+      this.loggedUserData = filteredData[0];
+      console.log(this.loggedUserData);
+
+    });
+  }
 }

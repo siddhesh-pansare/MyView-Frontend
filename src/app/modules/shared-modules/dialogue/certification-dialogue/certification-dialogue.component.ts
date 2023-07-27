b40e859f86ebe93d93cbe1782deb7fd117115c03
Anisha Singh <125570094+gem-AnishaSingh@users.noreply.github.com>
2023-07-27 11:15:38 +0530

    stash

---
+++ src/app/modules/shared-modules/dialogue/certification-dialogue/certification-dialogue.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-certification-dialogue',
+  templateUrl: './certification-dialogue.component.html',
+  styleUrls: ['./certification-dialogue.component.css']
+})
+export class CertificationDialogueComponent {
+
+}

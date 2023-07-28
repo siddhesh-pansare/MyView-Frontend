dfad4f6bb7d4bedb232ff3f1225a62e77324a148
gem-khushitiwari <khushi.tiwari@geminisolutions.com>
2023-07-28 18:13:45 +0530

    Awards dialogue box
    Developed the ui for the awards dialogue box

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './modules/shared-modules/shared-components/login/login.component';
+import { AwardComponent } from './modules/shared-modules/dialogue/awards-dialogue/award/award.component';
 
 const routes: Routes = [
-  { path:'', redirectTo: 'login', pathMatch:'full' },
-  { path:'login', component:LoginComponent }
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'award', component: AwardComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/modules/shared-modules/shared-modules.module.ts
+++ src/app/modules/shared-modules/shared-modules.module.ts
@@ -5,13 +5,15 @@ import { SharedModulesRoutingModule } from './shared-modules-routing.module';
 import { LoginComponent } from './shared-components/login/login.component';
 import { HeaderComponent } from './shared-components/header/header.component';
 import { SideBarComponent } from './shared-components/side-bar/side-bar.component';
+import { AwardComponent } from './dialogue/awards-dialogue/award/award.component';
 
 
 @NgModule({
   declarations: [
     LoginComponent,
     HeaderComponent,
-    SideBarComponent
+    SideBarComponent,
+    AwardComponent
   ],
   imports: [
     CommonModule,

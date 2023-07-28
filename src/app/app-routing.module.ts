896192eec91c7f476efcc0bd75882920a7c0ad11
Anisha Singh <125570094+gem-AnishaSingh@users.noreply.github.com>
2023-07-28 16:39:48 +0530

    interview_session_commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './modules/shared-modules/shared-components/login/login.component';
 import { DashboardComponent } from './modules/employee/dashboard/dashboard.component';
 import { CertificationDialogueComponent } from './modules/shared-modules/dialogue/certification-dialogue/certification-dialogue.component';
+import { InterviewComponent } from './modules/shared-modules/dialogue/interview-dialogue/interview/interview.component';
+import { SessionComponent } from './modules/shared-modules/dialogue/session-dialogue/session/session.component';
 
 const routes: Routes = [
   { path:'', redirectTo: 'login', pathMatch:'full' },
   { path:'login', component:LoginComponent },
   { path:'dashboard', component:DashboardComponent},
-  { path:'dialogue', component:CertificationDialogueComponent}
+  { path:'certification', component:CertificationDialogueComponent},
+  { path:'interview', component:InterviewComponent},
+  { path:'session', component:SessionComponent}
 ];
 
 @NgModule({
--- src/app/modules/shared-modules/shared-modules.module.ts
+++ src/app/modules/shared-modules/shared-modules.module.ts
@@ -5,15 +5,17 @@ import { SharedModulesRoutingModule } from './shared-modules-routing.module';
 import { LoginComponent } from './shared-components/login/login.component';
 import { HeaderComponent } from './shared-components/header/header.component';
 import { SideBarComponent } from './shared-components/side-bar/side-bar.component';
-import { CertificationDialogueComponent } from './shared-components/dialogue/certification-dialogue/certification-dialogue.component';
-
-
+import { CertificationDialogueComponent } from './dialogue/certification-dialogue/certification-dialogue.component';
+import { InterviewComponent } from './dialogue/interview-dialogue/interview/interview.component';
+import { SessionComponent } from './dialogue/session-dialogue/session/session.component';
 @NgModule({
   declarations: [
     LoginComponent,
     HeaderComponent,
     SideBarComponent,
-    CertificationDialogueComponent
+    CertificationDialogueComponent,
+    InterviewComponent,
+    SessionComponent
   ],
   imports: [
     CommonModule,

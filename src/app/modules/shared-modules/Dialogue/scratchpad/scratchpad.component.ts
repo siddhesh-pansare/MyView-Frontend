8842447f41aed2f674acd60f766a1fac40f41cc5
Shivanshu Khanna <Shivanshu.Khanna@Geminisolutions.com>
2023-07-30 15:18:27 +0530

    ipad screensize fixed

---
+++ src/app/modules/shared-modules/Dialogue/scratchpad/scratchpad.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-scratchpad',
+  templateUrl: './scratchpad.component.html',
+  styleUrls: ['./scratchpad.component.css']
+})
+export class ScratchpadComponent {
+
+}
--- src/app/modules/shared-modules/shared-modules.module.ts
+++ src/app/modules/shared-modules/shared-modules.module.ts
@@ -5,13 +5,15 @@ import { SharedModulesRoutingModule } from './shared-modules-routing.module';
 import { LoginComponent } from './shared-components/login/login.component';
 import { HeaderComponent } from './shared-components/header/header.component';
 import { SideBarComponent } from './shared-components/side-bar/side-bar.component';
+import { ScratchpadComponent } from './Dialogue/scratchpad/scratchpad.component';
 
 
 @NgModule({
   declarations: [
     LoginComponent,
     HeaderComponent,
-    SideBarComponent
+    SideBarComponent,
+    ScratchpadComponent
   ],
   imports: [
     CommonModule,

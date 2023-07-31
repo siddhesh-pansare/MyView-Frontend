63cf3f63db30d61c64fbc14b4a84cc69ddec9f87
Shobhit Singh <Shobhit.Singh@Geminisolutions.com>
2023-07-31 18:53:15 +0530

    employee table created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './modules/shared-modules/shared-components/login/login.component';
 import { HeaderComponent } from './modules/shared-modules/shared-components/header/header.component';
 import { AttendanceDialogComponent } from './modules/shared-modules/Dialog/attendance-dialog/attendance-dialog.component';
+import { EmployeesComponent } from './modules/employee/employees/employees.component';
 
 const routes: Routes = [
-  { path:'', redirectTo: 'attendance', pathMatch:'full' },
+  { path:'', redirectTo: 'employees', pathMatch:'full' },
   { path:'attendance', component:AttendanceDialogComponent },
-  {path:'header',component:HeaderComponent}
+  { path:'header',component:HeaderComponent },
+  { path:'employees',component:EmployeesComponent }
 ];
 
 @NgModule({
--- src/app/modules/employee/employee.module.ts
+++ src/app/modules/employee/employee.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { EmployeeRoutingModule } from './employee-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { EmployeesComponent } from './employees/employees.component';
 
 
 @NgModule({
   declarations: [
-    DashboardComponent
+    DashboardComponent,
+    EmployeesComponent
   ],
   imports: [
     CommonModule,

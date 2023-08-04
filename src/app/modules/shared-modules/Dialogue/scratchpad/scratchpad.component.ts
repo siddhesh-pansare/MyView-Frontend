8bb8d5e226b0a4df48de325806fa278a03769de5
Shivanshu Khanna <Shivanshu.Khanna@Geminisolutions.com>
2023-08-04 18:10:06 +0530

    animation added to scratchpad

--- src/app/modules/shared-modules/Dialogue/scratchpad/scratchpad.component.ts
+++ src/app/modules/shared-modules/Dialogue/scratchpad/scratchpad.component.ts
@@ -1,34 +1,54 @@
-import { Component } from '@angular/core';
-import { MatDialogRef, MatDialogConfig } from '@angular/material/dialog';
+import { Component, HostBinding } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+import { trigger, state, style, animate, transition, keyframes } from '@angular/animations';
 
 @Component({
   selector: 'app-scratchpad',
   templateUrl: './scratchpad.component.html',
-  styleUrls: ['./scratchpad.component.css']
+  styleUrls: ['./scratchpad.component.css'],
+  animations: [
+    trigger('expandCollapse', [
+      state(
+        'expanded',
+        style({
+          width: '{{expandedWidth}}',
+          height: '{{expandedHeight}}'
+        }),
+        { params: { expandedWidth: '550px', expandedHeight: '520px' } }
+      ),
+      state(
+        'collapsed',
+        style({
+          width: '{{originalWidth}}',
+          height: '{{originalHeight}}'
+        }),
+        { params: { originalWidth: '370px', originalHeight: '380px' } }
+      ),
+      // transition(
+      //   'expanded <=> collapsed',
+      //   animate(
+      //     '300ms',
+      //     keyframes([
+      //       style({ width: '{{originalWidth}}', height: '{{originalHeight}}', offset: 0 }),
+      //       style({ width: '{{expandedWidth}}', height: '{{expandedHeight}}', offset: 1 })
+      //     ])
+      //   )
+      // )
+      
+    ])
+  ]
 })
 export class ScratchpadComponent {
+  @HostBinding('@expandCollapse') public isExpanded = 'collapsed';
+
   private originalWidth = '370px';
   private originalHeight = '380px';
   private expandedWidth = '550px';
   private expandedHeight = '520px';
-  
-  public isExpanded = false;
 
-  constructor(public dialogRef: MatDialogRef<ScratchpadComponent>) {
+  constructor(public dialogRef: MatDialogRef<ScratchpadComponent>) {}
 
-    this.dialogRef.updateSize(this.originalWidth, this.originalHeight);
-   }
   expandDialog() {
-    const newWidth = this.isExpanded ? this.originalWidth : this.expandedWidth;
-    const newHeight = this.isExpanded ? this.originalHeight : this.expandedHeight;
-
-    // Update the dialog size
-    this.dialogRef.updateSize(newWidth, newHeight);
-
-    // Toggle the dialog state
-    this.isExpanded = !this.isExpanded;
-
-    
-    
+    this.isExpanded = this.isExpanded === 'collapsed' ? 'expanded' : 'collapsed';
   }
 }
--- src/app/modules/shared-modules/shared-components/side-bar/side-bar.component.ts
+++ src/app/modules/shared-modules/shared-components/side-bar/side-bar.component.ts
@@ -1,31 +1,44 @@
-import { Component } from '@angular/core';
-import {MatDialog, MatDialogConfig, MatDialogModule} from '@angular/material/dialog';
+import { Component, HostBinding, OnDestroy } from '@angular/core';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import { ScratchpadComponent } from '../../Dialogue/scratchpad/scratchpad.component';
-import { trigger,transition,style,animate } from "@angular/animations";
+import { trigger, transition, style, animate, AnimationEvent } from "@angular/animations";
 
 @Component({
   selector: 'app-side-bar',
   templateUrl: './side-bar.component.html',
-  styleUrls: ['./side-bar.component.css']
+  styleUrls: ['./side-bar.component.css'],
+  animations: [
+    trigger('slideInUp', [
+      transition(':enter', [
+        style({ transform: 'translateY(100%)' }),
+        animate('0ms linear', style({ transform: 'translateY(0)' }))
+      ])
+    ]),
+    trigger('slideOutDown', [
+      transition(':leave', [
+        animate('500ms linear', style({ transform: 'translateY(100%)' }))
+      ])
+    ])
+  ]
 })
 export class SideBarComponent {
-    constructor(private dialog:MatDialog){}
+  constructor(private dialog: MatDialog) { }
 
+  OpenScratch(enterAnimationDuration: string, exitAnimationDuration: string) {
+    const dialogConfig = new MatDialogConfig();
 
-    
-    OpenScratch(){
-      const dialogConfig = new MatDialogConfig();
-     
-      this.dialog.open(ScratchpadComponent,{
-        panelClass: ['animate__animated','animate__slideInUp'],
-        
-        
-        position: {right:'60px', bottom: '40px'},
-        backdropClass: 'cdk-overlay-transparent-backdrop',
-        
-        
-      })
-    
-    
-    }
+    const dialogRef = this.dialog.open(ScratchpadComponent, {
+      panelClass: ['animate__animated', 'animate__slideInUp'],
+      position: { right: '60px', bottom: '40px' },
+      backdropClass: 'cdk-overlay-transparent-backdrop',
+      enterAnimationDuration,
+      exitAnimationDuration,
+    });
+
+    // Subscribe to the beforeClosed event to handle the slideOutDown animation when the dialog is closing.
+    dialogRef.beforeClosed().subscribe(() => {
+      dialogRef.removePanelClass('animate__slideInUp'); // Remove the slideInUp class to prevent conflicts with the slideOutDown animation.
+      dialogRef.addPanelClass('animate__slideOutDown');
+    });
+  }
 }

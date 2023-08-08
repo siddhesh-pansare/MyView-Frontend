9086c5f2adb87cf1f5c8a76d82f6f39efabda3d1
Anisha Singh <125570094+gem-AnishaSingh@users.noreply.github.com>
2023-08-08 17:02:41 +0530

    dialoguebox_changes_anisha

--- src/app/modules/shared-modules/dialogue/session-dialogue/session.component.ts
+++ src/app/modules/shared-modules/dialogue/session-dialogue/session.component.ts
@@ -8,3 +8,16 @@ import { Component } from '@angular/core';
 export class SessionComponent {
 
 }
+// function toggleTab(tabId: string): void {
+//   const tabs = document.querySelectorAll('.tab-content');
+
+//   tabs.forEach((tab) => {
+//     const tabElement = tab as HTMLElement;
+//     if (tab.id === tabId) {
+//       tabElement.style.display = 'block';
+//     } else {
+//       tabElement.style.display = 'none';
+//     }
+//   });
+// }
+

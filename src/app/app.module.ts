469f1dd38a2e99bd4e72c4f15f42dadd825e0ccb
gem-khushitiwari <khushi.tiwari@geminisolutions.com>
2023-08-10 01:58:07 +0530

    Updates in Awards dialogue box.
    Updates in UI and data fetching inability.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MatTableModule } from '@angular/material/table';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    MatTableModule,
+    CommonModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/modules/shared-modules/dialogue/awards-dialogue/award/award.component.ts
+++ src/app/modules/shared-modules/dialogue/awards-dialogue/award/award.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+// award.component.ts
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+interface AwardData {
+  Date: string;
+  Award: string;
+  Nominated_By: string;
+}
 
 @Component({
   selector: 'app-award',
   templateUrl: './award.component.html',
-  styleUrls: ['./award.component.css']
+  styleUrls: ['./award.component.css'],
 })
-export class AwardComponent {
+export class AwardComponent implements OnInit {
+  awardData: AwardData[] = [];
+  displayedColumns: string[] = ['Date', 'Award', 'Nominated_By'];
+  isDataLoaded: boolean = false;
+  constructor(private http: HttpClient) {}
 
+  ngOnInit() {
+    this.fetchData();
+  }
+
+  fetchData(): void {
+    this.http
+      .get<{ awards: AwardData[] }>('./assets/data.json')
+      .subscribe((data) => {
+        this.awardData = data.awards;
+        this.isDataLoaded = true;
+      });
+  }
 }

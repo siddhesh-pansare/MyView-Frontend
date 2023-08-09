74719ab07630bc0df0a9f3717b46814f8442a074
siddhesh-pansare <122260522+siddhesh-pansare@users.noreply.github.com>
2023-08-09 18:42:53 +0530

    folders restructured

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './modules/shared-modules/shared-components/login/login.component';
-import { ClientFeedbackComponent } from './modules/shared-modules/shared-components/Dialog/client-feedback/client-feedback.component';
-import { OtherContributionsComponent } from './modules/shared-modules/shared-components/Dialog/other-contributions/other-contributions.component';
-import { RoundDetailsComponent } from './modules/shared-modules/shared-components/Dialog/round-details/round-details.component';
+import { ClientFeedbackComponent } from './modules/shared-modules/Dialogue/client-feedback/client-feedback.component';
+import { OtherContributionsComponent } from './modules/shared-modules/Dialogue/other-contributions/other-contributions.component';
+import { RoundDetailsComponent } from './modules/shared-modules/Dialogue/round-details/round-details.component';
 import { SideBarComponent } from './modules/shared-modules/shared-components/side-bar/side-bar.component';
 import { ScratchpadComponent } from './modules/shared-modules/Dialogue/scratchpad/scratchpad.component';
 import { LoaderComponent } from './modules/shared-modules/Dialogue/loader/loader.component';

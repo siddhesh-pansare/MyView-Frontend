51582af5779fdf1609ab21798a0d0abc1967531e
ADITYA KUMAR KUSHWAH <125469805+gem-AdityaKushwah@users.noreply.github.com>
2023-08-09 17:16:42 +0530

    login_component
    now responsive for different screen size

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { OtherContributionsComponent } from './modules/shared-modules/shared-com
 import { RoundDetailsComponent } from './modules/shared-modules/shared-components/Dialog/round-details/round-details.component';
 
 const routes: Routes = [
-  { path:'', redirectTo: 'round-details', pathMatch:'full' },
+  { path:'', redirectTo: 'login', pathMatch:'full' },
   { path:'login', component:LoginComponent },
   { path:'client-feedback',component:ClientFeedbackComponent},
   { path:'other-contributions',component:OtherContributionsComponent},

b7369c83a74bb0842b9758b00b98f904f3396855
ADITYA KUMAR KUSHWAH <125469805+gem-AdityaKushwah@users.noreply.github.com>
2023-08-11 17:20:08 +0530

    round_details

--- src/app/modules/shared-modules/Dialog/round-details/round-details.component.ts
+++ src/app/modules/shared-modules/Dialog/round-details/round-details.component.ts
@@ -1,8 +1,50 @@
 import { Component, OnInit } from '@angular/core';
 import{HttpClient} from '@angular/common/http';
 
+interface ProfileData {
+  Name: string;
+  Email: string;
+  Experience: string;
+  Status:string;
+}
 
-interface RoundDetails {
+interface ResumeScreening {
+  Date: string;
+  Step: string;
+  Status:string;
+  Interviewer:string;
+  Feedback: string;
+  Profile_Status:string;
+
+}
+interface OnlineTest {
+  Date: string;
+  Step: string;
+  Status:string;
+  Interviewer:string;
+  Feedback: string;
+  Profile_Status:string;
+
+}
+interface GroupDiscussion {
+  Date: string;
+  Step: string;
+  Status:string;
+  Interviewer:string;
+  Feedback: string;
+  Profile_Status:string;
+
+}
+interface Rround1 {
+  Date: string;
+  Step: string;
+  Status:string;
+  Interviewer:string;
+  Feedback: string;
+  Profile_Status:string;
+
+}
+interface Round2 {
   Date: string;
   Step: string;
   Status:string;
@@ -19,51 +61,79 @@ interface RoundDetails {
 })
 export class RoundDetailsComponent implements OnInit {
 
-  round1Data: RoundDetails[] = [];
-  round2Data: RoundDetails[] = [];
-  gdData: RoundDetails[] = [];
-  onlineTestData: RoundDetails[] = [];
-  resumeData: RoundDetails[] = [];
+  round1: Rround1[] = [];
+  round2: Round2[] = [];
+  groupdiscussion: GroupDiscussion[] = [];
+  onlineTest: OnlineTest[] = [];
+  resume: ResumeScreening[] = [];
+  profile:ProfileData[]=[];
+  activeTab: string = 'Resume Screening';
+  
 
   constructor(private http: HttpClient) {}
 
+  setActiveTab(tab: string) {
+    this.activeTab = tab;
+     this.fetchData(this.activeTab);
+  }
+
   ngOnInit() {
-    this.fetchRound1Data();
-    this.fetchRound2Data();
-    this.fetchGdData();
-    this.fetchOnlineTestData();
-    this.fetchResumeData();
+    this.fetchEmployeeProfile();
+    this.setActiveTab('Resume Screening');
+    
   }
 
-  fetchRound1Data() {
-    this.http.get<{ Round_1: RoundDetails[] }>('assets/temp_data/round_1.json').subscribe((data) => {
-      this.round1Data = data.Round_1;
+  fetchEmployeeProfile() {
+    this.http.get<{profile: ProfileData[] }>('assets/temp_data/user_profile.json').subscribe((data) => {
+    this.profile = data.profile;
+
     });
   }
 
-  fetchRound2Data() {
-    this.http.get<{ Round_2: RoundDetails[] }>('assets/temp_data/round_2.json').subscribe((data) => {
-      this.round2Data = data.Round_2;
+  fetchData(tab:string) {
+    let url = '';
+
+    if (tab === 'Resume Screening') {
+      url = 'assets/temp_data/resume_screening.json';
+    } else if (tab === 'Online Test') {
+      url = 'assets/temp_data/online_test.json';
+    } else if (tab === 'Group Discussion') {
+      url = 'assets/temp_data/group_discussion.json';
+    }else if (tab === 'Round 1') {
+      url = 'assets/temp_data/round_1.json';
+    }else if (tab === 'Round 2') {
+      url = 'assets/temp_data/round_2.json';
+    }
+    this.http.get<any>(url).subscribe((data) => {
+      if (tab === 'Resume Screening') {
+        this.resume = data.resume;
+      } else if (tab === 'Online Test') {
+        this.onlineTest = data.onlineTest;
+      } else if (tab === 'Group Discussion') {
+        this.groupdiscussion = data.groupdiscussion;
+      }else if (tab === 'Round 1') {
+        this.round1 = data.round1;
+      }
+      else if (tab === 'Round 2') {
+        this.round2 = data.round2;
+      }
     });
+  }
+  getStatusColor(status: string): string {
+    switch (status) {
+      case 'Fail':
+      case 'Not Match':
+      case 'Rejected':
+        return 'text-danger'; 
+      case 'Considered':
+      case 'Considered For Next Round':
+      case 'Match':
+        return 'text-success';
+      default:
+        return 'rgba(51, 54, 63, 0.60)'; 
+    }
   }
 
-  fetchGdData() {
-    this.http.get<{ Group_Discussion: RoundDetails[] }>('assets/temp_data/group_discussion.json').subscribe((data) => {
-      this.gdData = data.Group_Discussion;
-    });
-  }
-
-  fetchOnlineTestData() {
-    this.http.get<{ Online_Test: RoundDetails[] }>('assets/temp_data/online_test.json').subscribe((data) => {
-      this.onlineTestData = data.Online_Test;
-    });
-  }
-
-  fetchResumeData() {
-    this.http.get<{ Resume_screening: RoundDetails[] }>('assets/temp_data/resume_screening.json').subscribe((data) => {
-      this.resumeData = data.Resume_screening;
-    });
-  }
 }
 
 

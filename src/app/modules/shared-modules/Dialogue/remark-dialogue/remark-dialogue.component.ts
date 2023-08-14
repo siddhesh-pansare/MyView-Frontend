743904613eec09cac79b7ab53d76fe629af923a0
Ayush Mittal <122875758+gem-ayushmittal@users.noreply.github.com>
2023-08-14 18:34:06 +0530

    remarks dialogue correct

--- src/app/modules/shared-modules/Dialogue/remark-dialogue/remark-dialogue.component.ts
+++ src/app/modules/shared-modules/Dialogue/remark-dialogue/remark-dialogue.component.ts
@@ -3,8 +3,9 @@ import { HttpClient } from '@angular/common/http';
 
 interface RemarkData {
   Date: string;
-  Feedback: string;
-  editable: boolean;
+  Feedback: string;  
+  Action: string;
+  Action1: string;
 }
 
 @Component({
@@ -15,6 +16,7 @@ interface RemarkData {
 export class RemarkDialogueComponent implements OnInit {
   remarkdata: RemarkData[] = [];
   displayedColumns: string[] = ['Date', 'Feedback'];
+  isDataLoaded: boolean = false;
 
   constructor(private http: HttpClient) { }
 
@@ -22,17 +24,18 @@ export class RemarkDialogueComponent implements OnInit {
     this.fetchData();
   }
 
-  fetchData() {
+  fetchData():void {
     this.http.get<{ remarks: RemarkData[] }>('assets/temp_data/remarks.json').subscribe((data) => {
       this.remarkdata = data.remarks;
+      this.isDataLoaded=this.remarkdata.length > 0;
     });
   }
-
+  
 
   deleteRow(index: number) {
     // Remove the row from the array based on the given index
     this.remarkdata.splice(index, 1);
   };
-
-
+  
+  
 }

285466476b736d26ee5847f448c0ca3a66a44207
siddhesh-pansare <122260522+siddhesh-pansare@users.noreply.github.com>
2023-08-14 10:20:33 +0530

    Associates duplicates avoided

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -26,7 +26,6 @@ import { LoginComponent } from './modules/shared-modules/shared-components/login
 import { DashboardComponent } from './modules/employee/dashboard/dashboard.component';
 import { RoleComponent } from './modules/shared-modules/shared-components/role/role.component';
 import { PseudoDashboardComponent } from './modules/employee/pseudo-dashboard/pseudo-dashboard.component';
-import { ReporteesComponent } from './modules/manager/AssociatesComponent/reportees/reportees.component';
 import { ECInformationComponent } from './modules/employee/dashboard/ec-information/ec-information.component';
 import { DcInformationComponent } from './modules/employee/dashboard/dc-information/dc-information.component';
 
@@ -43,7 +42,6 @@ const routes: Routes = [
     }
   ]
 },
-{path:'reportees', component:ReporteesComponent},
 {path:'ec',component:ECInformationComponent},
 {path:'dc', component:DcInformationComponent},
 {path:'dashboard', component:DashboardComponent}

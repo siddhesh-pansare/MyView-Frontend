89036818fa05b1dc407cbbd6ca3e8b664489307a
Shivanshu Khanna <Shivanshu.Khanna@Geminisolutions.com>
2023-08-17 18:13:25 +0530

    button conflict
    button not working

--- src/app/modules/employee/dashboard/dashboard.component.ts
+++ src/app/modules/employee/dashboard/dashboard.component.ts
@@ -10,12 +10,27 @@ import { InterviewComponent } from '../../shared-modules/dialogue/interview-dial
 import { SessionComponent } from '../../shared-modules/dialogue/session-dialogue/session.component';
 import { ClientFeedbackComponent } from '../../shared-modules/Dialogue/client-feedback/client-feedback.component';
 import { OtherContributionsComponent } from '../../shared-modules/Dialogue/other-contributions/other-contributions.component';
+import { animate, style, transition, trigger } from '@angular/animations';
+import { ScratchpadComponent } from '../../shared-modules/Dialogue/scratchpad/scratchpad.component';
 // import { ClientFeedbackComponent } from '../../shared-modules/Dialogue/client-feedback/client-feedback.component';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.css'],
+  animations: [
+    trigger('slideInUp', [
+      transition(':enter', [
+        style({ transform: 'translateY(100%)' }),
+        animate('0ms linear', style({ transform: 'translateY(0)' }))
+      ])
+    ]),
+    trigger('slideOutDown', [
+      transition(':leave', [
+        animate('500ms linear', style({ transform: 'translateY(100%)' }))
+      ])
+    ])
+  ]
 })
 export class DashboardComponent implements OnInit {
 
@@ -86,7 +101,23 @@ export class DashboardComponent implements OnInit {
       }
     });
   }
+  OpenScratch(enterAnimationDuration: string, exitAnimationDuration: string) {
+    const dialogConfig = new MatDialogConfig();
 
+    const dialogRef = this.dialog.open(ScratchpadComponent, {
+      panelClass: ['animate__animated', 'animate__slideInUp'],
+      position: { right: '3%', bottom: '40px' },
+      backdropClass: 'cdk-overlay-transparent-backdrop',
+      enterAnimationDuration,
+      exitAnimationDuration,
+    });
+
+    // Subscribe to the beforeClosed event to handle the slideOutDown animation when the dialog is closing.
+    dialogRef.beforeClosed().subscribe(() => {
+      dialogRef.removePanelClass('animate__slideInUp'); // Remove the slideInUp class to prevent conflicts with the slideOutDown animation.
+      dialogRef.addPanelClass('animate__slideOutDown');
+    });
+  }
   openDialog(dialogComponent: string){
     if(dialogComponent === 'AwardComponent'){
       const dialogRef = this.dialog.open(AwardComponent, {

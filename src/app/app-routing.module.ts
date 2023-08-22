ec18a2a9d6d24e61edbd5ce0df1f1eb80bb7baca
ADITYA KUMAR KUSHWAH <125469805+gem-AdityaKushwah@users.noreply.github.com>
2023-08-22 15:11:50 +0530

    client-feedack

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { OtherContributionsComponent } from './modules/shared-modules/Dialogue/o
 import { RoundDetailsComponent } from './modules/shared-modules/Dialogue/round-details/round-details.component';
 
 const routes: Routes = [
-  { path:'', redirectTo: 'round-details', pathMatch:'full' },
+  { path:'', redirectTo: 'login', pathMatch:'full' },
   { path:'login', component:LoginComponent },
   { path:'client-feedback',component:ClientFeedbackComponent},
   { path:'other-contributions',component:OtherContributionsComponent},

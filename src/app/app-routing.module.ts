e5cc8e11e5e79a65957009aa8c552c8e6ba51cac
Ayush Mittal <122875758+gem-ayushmittal@users.noreply.github.com>
2023-08-22 12:46:22 +0530

    reportee responsive

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -21,6 +21,7 @@ const routes: Routes = [
   { path:'login', component:LoginComponent },
   { path:'p-dashboard', component:PseudoDashboardComponent },
   {path:'remarks',component:RemarkDialogueComponent},
+  {path:'reportees',component:ReporteesComponent},
   { path:'', component:RoleComponent ,
   children:[
     {

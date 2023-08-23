1c634c9253d386e8657d0b16661a36b66629481a
gem-khushitiwari <khushi.tiwari@geminisolutions.com>
2023-08-23 07:58:40 +0530

    Style changes in header UI

--- src/app/modules/employee/dashboard/dc-information/dc-information.component.ts
+++ src/app/modules/employee/dashboard/dc-information/dc-information.component.ts
@@ -1,38 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 import { LoggedUserDataService } from 'src/app/services/logged-user-data.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-dc-information',
   templateUrl: './dc-information.component.html',
-  styleUrls: ['./dc-information.component.css']
+  styleUrls: ['./dc-information.component.css'],
 })
-export class DcInformationComponent implements OnInit{
-
-  loggedUserData: any;     //variable to store complete data from MIS
-  collapseClient : boolean = true;
-  collapseReportee : boolean = true;
+export class DcInformationComponent implements OnInit {
+  loggedUserData: any; //variable to store complete data from MIS
+  collapseClient: boolean = true;
+  collapseReportee: boolean = true;
   selectedCouncil: string = 'pimco-de';
-  constructor(private userDataService: LoggedUserDataService ){
+  constructor(private userDataService: LoggedUserDataService) {
     const email = 'siddhesh.pansare@geminisolutions.com';
 
     this.userDataService.fetchDataByEmail(email).subscribe((filteredData) => {
       this.loggedUserData = filteredData[0];
       console.log(this.loggedUserData);
-
     });
   }
-
 
   ngOnInit(): void {
     //this.loadData();
   }
 
-  loadData(): void {
+  isDropdownOpen: boolean = false;
 
+  toggleDropdown() {
+    this.isDropdownOpen = !this.isDropdownOpen;
   }
 
-  toggleClient(){
+  loadData(): void {}
+
+  toggleClient() {
     this.collapseClient = !this.collapseClient;
   }
-
 }

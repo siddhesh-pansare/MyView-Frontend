bf6144a67f988dce284d7e06c5879ac144736bdb
Shobhit Singh <Shobhit.Singh@Geminisolutions.com>
2023-08-23 11:48:34 +0530

    Revert "Style changes in header UI"
    This reverts commit 1c634c9253d386e8657d0b16661a36b66629481a.

--- src/app/modules/employee/dashboard/dc-information/dc-information.component.ts
+++ src/app/modules/employee/dashboard/dc-information/dc-information.component.ts
@@ -1,39 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 import { LoggedUserDataService } from 'src/app/services/logged-user-data.service';
-import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-dc-information',
   templateUrl: './dc-information.component.html',
-  styleUrls: ['./dc-information.component.css'],
+  styleUrls: ['./dc-information.component.css']
 })
-export class DcInformationComponent implements OnInit {
-  loggedUserData: any; //variable to store complete data from MIS
-  collapseClient: boolean = true;
-  collapseReportee: boolean = true;
+export class DcInformationComponent implements OnInit{
+
+  loggedUserData: any;     //variable to store complete data from MIS
+  collapseClient : boolean = true;
+  collapseReportee : boolean = true;
   selectedCouncil: string = 'pimco-de';
-  constructor(private userDataService: LoggedUserDataService) {
+  constructor(private userDataService: LoggedUserDataService ){
     const email = 'siddhesh.pansare@geminisolutions.com';
 
     this.userDataService.fetchDataByEmail(email).subscribe((filteredData) => {
       this.loggedUserData = filteredData[0];
       console.log(this.loggedUserData);
+
     });
   }
+
 
   ngOnInit(): void {
     //this.loadData();
   }
 
-  isDropdownOpen: boolean = false;
+  loadData(): void {
 
-  toggleDropdown() {
-    this.isDropdownOpen = !this.isDropdownOpen;
   }
 
-  loadData(): void {}
-
-  toggleClient() {
+  toggleClient(){
     this.collapseClient = !this.collapseClient;
   }
+
 }

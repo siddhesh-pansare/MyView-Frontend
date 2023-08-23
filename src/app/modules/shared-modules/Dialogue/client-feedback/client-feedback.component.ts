8dd6af4504e8cefa7dcc3a7a0e0b3686d8031835
ADITYA KUMAR KUSHWAH <125469805+gem-AdityaKushwah@users.noreply.github.com>
2023-08-23 12:27:13 +0530

    changes in .ts

--- src/app/modules/shared-modules/Dialogue/client-feedback/client-feedback.component.ts
+++ src/app/modules/shared-modules/Dialogue/client-feedback/client-feedback.component.ts
@@ -1,5 +1,7 @@
 import { Component,OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
 
 interface FeedbackData {
   Date: string;
@@ -18,8 +20,8 @@ export class ClientFeedbackComponent implements OnInit {
 
   feedbackdata: FeedbackData[] = [];
   displayedColumns: string[] = ['Date', 'Feedback'];
+  constructor(private http: HttpClient, private snackBar: MatSnackBar) {}
 
-  constructor(private http: HttpClient) { }
 
   ngOnInit() {
     this.fetchData();
@@ -28,7 +30,31 @@ export class ClientFeedbackComponent implements OnInit {
   fetchData() {
     this.http.get<{ feedback: FeedbackData[] }>('assets/temp_data/client_feedback.json').subscribe((data) => {
       this.feedbackdata = data.feedback;
+    }, (error) => {
+      console.error('Error fetching data:', error);
+      if (error.status === 404) {
+        this.snackBar.open('Data not found (404 error).', 'Close', {
+          duration: 5000,
+        });
+      } else if (error.status === 0) {
+        this.snackBar.open(
+          'Connectivity issues. Please check your network connection.',
+          'Close',
+          {
+            duration: 5000,
+          }
+        );
+      } else {
+        this.snackBar.open(
+          'An error occurred while fetching data.',
+          'Close',
+          {
+            duration: 5000,
+          }
+        );
+      }
     });
+    
   }
 
   deleteRow(index: number) {
--- src/app/modules/shared-modules/Dialogue/other-contributions/other-contributions.component.ts
+++ src/app/modules/shared-modules/Dialogue/other-contributions/other-contributions.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+
 
 interface ContributionData {
   Date: string;
@@ -17,7 +20,7 @@ export class OtherContributionsComponent implements OnInit {
   displayedColumns: string[] = ['Date', 'Details'];
   hasContributions: boolean = false; // New property to track if there are contributions
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private snackBar: MatSnackBar) {}
 
   ngOnInit() {
     this.fetchData();
@@ -28,6 +31,29 @@ export class OtherContributionsComponent implements OnInit {
       this.contributions = data.contributions;
       
       this.hasContributions = this.contributions.length > 0; // Update the hasContributions property
+    }, (error) => {
+      console.error('Error fetching data:', error);
+      if (error.status === 404) {
+        this.snackBar.open('Data not found (404 error).', 'Close', {
+          duration: 5000,
+        });
+      } else if (error.status === 0) {
+        this.snackBar.open(
+          'Connectivity issues. Please check your network connection.',
+          'Close',
+          {
+            duration: 5000,
+          }
+        );
+      } else {
+        this.snackBar.open(
+          'An error occurred while fetching data.',
+          'Close',
+          {
+            duration: 5000,
+          }
+        );
+      }
     });
   }
   deleteRow(index: number) {

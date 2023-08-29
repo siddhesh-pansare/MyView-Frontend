eacaf3794070ba3af24b2a16fc1b439007a76127
siddhesh-pansare <122260522+siddhesh-pansare@users.noreply.github.com>
2023-08-29 06:16:28 +0530

    work completed

--- src/app/modules/employee/pseudo-dashboard/pseudo-dashboard.component.ts
+++ src/app/modules/employee/pseudo-dashboard/pseudo-dashboard.component.ts
@@ -15,112 +15,71 @@ import { InterviewComponent } from '../interview/interview.component';
 })
 export class PseudoDashboardComponent implements OnInit {
 
-
   //declare variables
   loggedUserData: any;     //variable to store complete data from MIS
   selectedTab = 'EC';
   activeTab: string = 'dc';
 
   cards: { cardImageSrc: string; cardTitle: string, ImageAlt: string, dialogComponent: string }[] = [
-    { cardImageSrc: '../../../../assets/images/icons/awards.svg', cardTitle: 'Awards', ImageAlt:'award', dialogComponent:'AwardComponent' },
-    { cardImageSrc: '../../../../assets/images/icons/certificate.svg', cardTitle: 'Certifications', ImageAlt:'certification',dialogComponent:'CertificationComponent' },
-    { cardImageSrc: '../../../../assets/images/icons/interview.svg', cardTitle: 'Interviews', ImageAlt:'interviews', dialogComponent:'InterviewsComponent' },
-    { cardImageSrc: '../../../../assets/images/icons/session.svg', cardTitle: 'Sessions', ImageAlt:'sessions', dialogComponent:'SessionComponent' },
-    { cardImageSrc: '../../../../assets/images/icons/client_feedback.svg', cardTitle: 'Client Feedback', ImageAlt:'client_feedback', dialogComponent:'ClientFeedbackComponent' },
-    { cardImageSrc: '../../../../assets/images/icons/other_contributions.svg', cardTitle: 'Other Contributions', ImageAlt:'other-Contributions',dialogComponent:'OtherContributionsComponent' },
+    { cardImageSrc: '../../../../assets/images/icons/awards.svg', cardTitle: 'Awards', ImageAlt: 'award', dialogComponent: 'AwardComponent' },
+    { cardImageSrc: '../../../../assets/images/icons/certificate.svg', cardTitle: 'Certifications', ImageAlt: 'certification', dialogComponent: 'CertificationComponent' },
+    { cardImageSrc: '../../../../assets/images/icons/interview.svg', cardTitle: 'Interviews', ImageAlt: 'interviews', dialogComponent: 'InterviewsComponent' },
+    { cardImageSrc: '../../../../assets/images/icons/session.svg', cardTitle: 'Sessions', ImageAlt: 'sessions', dialogComponent: 'SessionComponent' },
+    { cardImageSrc: '../../../../assets/images/icons/client_feedback.svg', cardTitle: 'Client Feedback', ImageAlt: 'client_feedback', dialogComponent: 'ClientFeedbackComponent' },
+    { cardImageSrc: '../../../../assets/images/icons/other_contributions.svg', cardTitle: 'Other Contributions', ImageAlt: 'other-Contributions', dialogComponent: 'OtherContributionsComponent' },
   ];
+  col4InitialX!: number; // Store the initial x-coordinate of the col-md-4 column
 
-  handleClick(event: MouseEvent): void {
-    const xCoordinate = event.clientX;
-    const yCoordinate = event.clientY;
-
-    console.log(`Mouse clicked at X: ${xCoordinate}, Y: ${yCoordinate}`);
-  }
-
-  constructor(private dialog: MatDialog ){}
-
-
-
+  constructor() { }
 
   ngOnInit(): void {
+    this.col4InitialX = this.getCol4InitialX(); // Store the initial x-coordinate
+    this.restorePositions(); // Restore positions on page load
   }
 
-  activateTab(tab: string) {
-    this.activeTab = tab;
+  getCol4InitialX(): number {
+    const col4 = document.querySelector('.col-md-4');
+    console.log(col4?.getBoundingClientRect().left);
+
+    return col4 ? col4.getBoundingClientRect().left : 0;
   }
 
-  ngAfterViewInit(): void {
-    this.restoreBoxPositions();
+  onDragEnd(event: CdkDragEnd, elementId: string, inCol8: boolean): void {
+    const element = event.source.getRootElement();
+    const rect = element.getBoundingClientRect();
+    let coordinates = { x: rect.left, y: rect.top };
+
+
+
+    if (inCol8) {
+      //if element is from col-md-8
+      console.log("inside @incol8");
+      localStorage.setItem(elementId, JSON.stringify(coordinates));
+    }else{
+      coordinates = {x:(coordinates.x - this.getCol4InitialX()), y:rect.top}
+      localStorage.setItem(elementId, JSON.stringify(coordinates));
+    }
   }
 
+  restorePositions(): void {
+    const elements = document.querySelectorAll('.example-box');
 
+    elements.forEach(element => {
+      const elementId = element.id;
+      const storedCoordinates = localStorage.getItem(elementId);
 
-  selectTab(tab: string) {
-    this.selectedTab = tab;
-  }
-
-  drop(event: CdkDragDrop<{ title: string; poster: string }[]>) {
-    moveItemInArray(this.cards, event.previousIndex, event.currentIndex);
-  }
-
-  private getAndSaveBoxCoordinates(): void {
-    const boxes: NodeListOf<HTMLElement> = document.querySelectorAll('.example-box');
-
-    boxes.forEach((box: HTMLElement, index: number) => {
-      const boxRect = box.getBoundingClientRect();
-      const boxCoordinates = { left: boxRect.left, top: boxRect.top };
-      localStorage.setItem(`box${index + 1}_coordinates`, JSON.stringify(boxCoordinates));
-    });
-  }
-
-  onDragEnded(event: any) {
-    this.getAndSaveBoxCoordinates();
-  }
-
-  private restoreBoxPositions(): void {
-    const boxes: NodeListOf<HTMLElement> = document.querySelectorAll('.example-box');
-
-    boxes.forEach((box: HTMLElement, index: number) => {
-      const storedCoordinates = localStorage.getItem(`box${index + 1}_coordinates`);
       if (storedCoordinates) {
-        const { left, top } = JSON.parse(storedCoordinates);
-        box.style.position = 'absolute'; // Use absolute positioning
-        box.style.display = 'block';
-        box.style.left = `${left}px`;
-        box.style.top = `${top}px`;
+        const coordinates = JSON.parse(storedCoordinates);
+        const htmlElement = element as HTMLElement;
+
+        // Set the element's position to absolute for accurate positioning
+        htmlElement.style.position = 'absolute';
+
+        // Set the saved coordinates
+        htmlElement.style.left = `${coordinates.x}px`;
+        htmlElement.style.top = `${coordinates.y}px`;
       }
     });
   }
-
-  openDialog(dialogComponent: string){
-    if(dialogComponent === 'AwardComponent'){
-      const dialogRef = this.dialog.open(AwardComponent, {
-        width: '400px', // Set the width as needed
-      });
-    }else if(dialogComponent === 'CertificationComponent'){
-      const dialogRef = this.dialog.open(CertificationDialogueComponent, {
-        width: '400px', // Set the width as needed
-      });
-    }else if(dialogComponent === 'InterviewsComponent'){
-      const dialogRef = this.dialog.open(InterviewComponent, {
-        width: '400px', // Set the width as needed
-      });
-    }
-    else if(dialogComponent === 'SessionComponent'){
-      const dialogRef = this.dialog.open(SessionComponent, {
-        width: '400px', // Set the width as needed
-      });
-    }
-    else if(dialogComponent === 'ClientFeedbackComponent'){
-      const dialogRef = this.dialog.open(ClientFeedbackComponent, {
-        width: '400px', // Set the width as needed
-      });
-    }
-    else if(dialogComponent === 'OtherContributionsComponent'){
-      const dialogRef = this.dialog.open(OtherContributionsComponent, {
-        width: '400px', // Set the width as needed
-      });
-    }
-  }
-
 }
+

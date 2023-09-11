3cea6099d84bb882fbe6b3580087d791b286ef5c
gem-khushitiwari <khushi.tiwari@geminisolutions.com>
2023-09-11 12:07:05 +0530

    Dc info
    added the reportees screen as a popup

--- src/app/modules/employee/dashboard/dc-information/dc-information.component.ts
+++ src/app/modules/employee/dashboard/dc-information/dc-information.component.ts
@@ -1,38 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import { ReporteesComponent } from 'src/app/modules/manager/associates-component/reportees/reportees.component';
 import { LoggedUserDataService } from 'src/app/services/logged-user-data.service';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-dc-information',
   templateUrl: './dc-information.component.html',
-  styleUrls: ['./dc-information.component.css']
+  styleUrls: ['./dc-information.component.css'],
 })
-export class DcInformationComponent implements OnInit{
+export class DcInformationComponent implements OnInit {
+  loggedUserData: any; //variable to store complete data from MIS
+  collapseClient: boolean = true;
+  collapseReportee: boolean = true;
 
-  loggedUserData: any;     //variable to store complete data from MIS
-  collapseClient : boolean = true;
-  collapseReportee : boolean = true;
-
-  constructor(private userDataService: LoggedUserDataService ){
+  constructor(
+    private userDataService: LoggedUserDataService,
+    private dialog: MatDialog
+  ) {
     const email = 'siddhesh.pansare@geminisolutions.com';
 
     this.userDataService.fetchDataByEmail(email).subscribe((filteredData) => {
       this.loggedUserData = filteredData[0];
       console.log(this.loggedUserData);
-
     });
   }
-
 
   ngOnInit(): void {
     //this.loadData();
   }
 
-  loadData(): void {
+  loadData(): void {}
 
-  }
-
-  toggleClient(){
+  toggleClient() {
     this.collapseClient = !this.collapseClient;
   }
 
+  openReporteesDialog() {
+    this.dialog.open(ReporteesComponent, {
+      panelClass: 'custom-dialog-class',
+      width: 'auto', // Set the width as needed, or remove this line to
+    });
+  }
 }
